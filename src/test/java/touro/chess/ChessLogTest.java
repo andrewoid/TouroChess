97fc8a1b3d277ec580262b65836f66c54538ba6a
HadassahR <58051299+HadassahR@users.noreply.github.com>
2021-10-11 22:10:45 -0400

    Removed commented code

--- src/test/java/touro/chess/ChessLogTest.java
+++ src/test/java/touro/chess/ChessLogTest.java
@@ -26,31 +26,5 @@ public class ChessLogTest {
         Assert.assertTrue(chessLog.getWhiteMoves().isEmpty());
         Assert.assertTrue(chessLog.getBlackMoves().size() == 1);
     }
-
-
-
-//    @Test
-//    public void getMoves() {
-//        //given
-//        Location startPosition = new Location(4,4);
-//        KingPiece king = new KingPiece(startPosition, PieceColor.Black);
-//        List<Move>practiceList = new ArrayList<>();
-//        practiceList.add(new Move(startPosition,new Location(5,4),false));
-//        practiceList.add(new Move(startPosition,new Location(3,4),false));
-//        practiceList.add(new Move(startPosition,new Location(4,5),false));
-//        practiceList.add(new Move(startPosition,new Location(4,3),false));
-//        practiceList.add(new Move(startPosition,new Location(5,3),false));
-//        practiceList.add(new Move(startPosition,new Location(3,3),false));
-//        practiceList.add(new Move(startPosition,new Location(5,5),false));
-//        practiceList.add(new Move(startPosition,new Location(3,5),false));
-//
-//        //when
-//        List<Move> legalMoves = king.getMoves();
-//
-//        //then
-//        for (int i = 0; i<=7; i++){
-//            assertEquals(legalMoves.get(i).getTo().getColumn(),practiceList.get(i).getTo().getColumn());
-//            assertEquals(legalMoves.get(i).getTo().getRow(),practiceList.get(i).getTo().getRow());
-//        }
-//    }
+    
 }
